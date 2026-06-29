b31031ed5a939f5ffc92b3c90bd6e7ac6216a415
Darvesh Soni <darveshsoni1@gmail.com>
2026-06-29 19:12:17 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length!==t.length) return false
+        let map = {};
+        for(let i=0; i<s.length; i++){
+          if(!map[s[i]]){
+            map[s[i]] = 1;
+          }else{
+            map[s[i]]++;
+          }
+        }
+
+        for(let j=0; j<t.length; j++){
+             if(map[t[j]]){
+                map[t[j]]--;
+             }else{
+                return false
+             }
+        }
+   return true;
+    }
+}
