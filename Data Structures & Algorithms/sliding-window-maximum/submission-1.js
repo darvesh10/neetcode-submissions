d3a2e7c85c1a86eb56c6670833310ab895df5286
Darvesh Soni <darveshsoni1@gmail.com>
2026-07-10 19:44:03 +0530

    Add: sliding-window-maximum - submission-1

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    maxSlidingWindow(nums, k) {
+        let i = 0;
+        let j = 0;
+        let q = [];
+        let res = [];
+        while(j<nums.length){
+            while(q.length && nums[j]>q[q.length-1]) q.pop();
+            q.push(nums[j]);
+            if(j>=k-1){res.push(q[0]);
+                   if(nums[i]===q[0]) q.shift();
+                   i++; 
+            }
+            j++;
+        }
+        return res;
+    }
+}
