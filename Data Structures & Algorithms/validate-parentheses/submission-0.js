22640a4d0a090f0e50b77b00b9d76f3932e04fed
Darvesh Soni <darveshsoni1@gmail.com>
2026-07-11 14:40:49 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let map = {
+            '{' : '}',
+            '[' : ']',
+            '(' : ')'
+        }
+        let stack = [];
+
+        for(let i=0; i<s.length; i++){
+            if(s[i]==='(' || s[i]==='[' || s[i]==='{'){
+                stack.push(s[i])
+            }else{
+                if(map[stack.pop()]!==s[i]) return false
+            }
+        }
+        return stack.length===0;
+    }
+}
